d668fc8dbef7ead6d3dbf952bf255fed43dd294b
ditedetian <ditadetyan@gmail.com>
2023-09-21 21:19:48 +0700

    use react-infinite-scroll-component

--- src/App.js
+++ src/App.js
@@ -2,31 +2,41 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Body from './Body';
-import MovieDetail from './MovieDetail'; // Import the MovieDetail component
+import MovieDetail from './MovieDetail';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-
-// Import Tailwind CSS
 import 'tailwindcss/tailwind.css';
-
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-
   const [page, setPage] = useState(1);
   const bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjNlYzkxZDM1MTYyZGQ0ZmE5OTY3ODQ1ODIwMTJmZCIsInN1YiI6IjYxZmI0ZGVmNDE0MjkxMDBhMjE3MmM1OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GPyOocPqIdDWplpMcoOBL7h3htPH6STZhNZn0tFfuDc';
-
   const apiUrl = 'https://api.themoviedb.org/3/discover/movie';
-  const queryParams = new URLSearchParams({
+
+  // Define handleScroll outside of useEffect
+  const handleScroll = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+
+  // Define fetchMovies outside of useEffect
+  const fetchMovies = (pageToFetch) => {
+    setLoading(true);
+    const newPage = pageToFetch || page;
+    const queryParams = new URLSearchParams({
       include_adult: false,
       include_video: false,
       language: 'en-US',
-      page: page,
+      page: newPage,
       sort_by: 'popularity.desc',
-  });
-  const fetchMovies = () => {
-    setLoading(true); // Menampilkan indikator loading
+    });
+
     fetch(`${apiUrl}?${queryParams.toString()}`, {
       method: 'GET',
       headers: {
@@ -37,50 +47,54 @@ function App() {
     })
     .then(response => response.json())
     .then(data => {
-      setMovies(prevMovies => [...prevMovies, ...data.results]);
-      setLoading(false); // Menyembunyikan indikator loading setelah selesai
+      if (data.results.length > 0) {
+        setMovies(prevMovies => [...prevMovies, ...data.results]);
+        setLoading(false);
+      } else {
+        window.removeEventListener('scroll', handleScroll);
+      }
     })
     .catch(error => {
       console.error('Error:', error);
-      setLoading(false); // Menyembunyikan indikator loading jika terjadi kesalahan
+      setLoading(false);
     });
   };
 
   useEffect(() => {
-      fetchMovies();
+    fetchMovies();
   }, [page]);
 
-  // Mengamati event scroll window untuk mengambil lebih banyak data saat mencapai bawah halaman
   useEffect(() => {
-      const handleScroll = () => {
-          if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight)
-          {
-            setPage(prevPage => prevPage + 1);
-          }
-        };
-      window.addEventListener('scroll', handleScroll);
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-  }, []);
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // Empty dependency array to run only once
 
   const handleSearch = (searchResults) => {
-    // Setel hasil pencarian ke state movies
     setMovies(searchResults);
   };
 
   return (
-  <>
-    <Header onSearch={handleSearch} />
+    <>
+      <Header onSearch={handleSearch} />
       <Router>
         <Routes className="body">
-          <Route path="/" element={<Body movies={movies} loading={loading}/>} />
+          <Route path="/" element={
+            <InfiniteScroll
+              dataLength={movies.length}
+              next={() => fetchMovies(page + 1)}
+              hasMore={!loading}
+              loader={<h4>Loading...</h4>}
+            >
+              <Body movies={movies} loading={loading} />
+            </InfiniteScroll>
+          } />
           <Route path="/movie/:id" element={<MovieDetail />} />
         </Routes>
       </Router>
-
-  </>
-);
+    </>
+  );
 }
 
 export default App;
